0c22bded6290d7910f9b4788943198999fd304b9
RoziqovM <mroziqov494@gmail.com>
2024-04-19 03:06:55 +0500

    commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import LoginForm from "./components/LoginForm";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <LoginForm />
+      </div>
+    </>
+  );
+}
+
+export default App;
