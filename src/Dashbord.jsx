a3cca15660573856c46cc34cb7dd3d8470a1fa30
RoziqovM <mroziqov494@gmail.com>
2024-04-21 14:59:48 +0500

    commit

--- src/Dashbord.jsx
+++ src/Dashbord.jsx
@@ -1,6 +1,14 @@
 import React, { useState } from "react";
 import LoginForm from "./LoginPages/LoginForm";
 import DataTable from "./components/DataTable";
+import { LuShoppingCart } from "react-icons/lu";
+import {
+  MdOutlinePeople,
+  MdRadioButtonChecked,
+  MdInsertChartOutlined,
+} from "react-icons/md";
+import { IoIosNotificationsOutline, IoSettingsOutline } from "react-icons/io";
+import Avatar from "@mui/material/Avatar";
 
 const Dashboard = () => {
   const [selectedOption, setSelectedOption] = useState(null);
@@ -11,42 +19,50 @@ const Dashboard = () => {
 
   return (
     <div className="flex items-center justify-center bg-[#DCE9F9]">
-      <div className="fixed  left-0 top-0 flex h-full w-32 flex-col justify-between bg-gray-800">
-        <div className="mt-4 flex flex-col items-center bg-[#F1F4F8]">
+      <div className="fixed left-0 top-0 flex h-full w-24 flex-col items-center justify-between bg-gray-800 p-5">
+        <div className="mt-4 flex w-10 flex-col items-center rounded-lg bg-[#717377] p-3">
           <button
-            className={`mb-4 text-lg text-white ${selectedOption === "option1" ? "bg-blue-500" : ""}`}
-            onClick={() => handleOptionClick("option1")}
+            className={`mb-4 text-lg text-white ${selectedOption === "DataTable" ? "text-blue-500" : ""}`}
+            onClick={() => handleOptionClick("DataTable")}
           >
-            Option 1
+            <LuShoppingCart />
           </button>
           <button
-            className={`mb-4 text-lg text-white ${selectedOption === "option2" ? "bg-blue-500" : ""}`}
-            onClick={() => handleOptionClick(<DataTable />)}
+            className={`mb-4 text-lg text-white ${selectedOption === "option2" ? "text-blue-700" : ""}`}
+            onClick={() => handleOptionClick("option2")}
           >
-            Option 2
+            <MdOutlinePeople />
           </button>
           <button
-            className={`mb-4 text-lg text-white ${selectedOption === "option3" ? "bg-blue-500" : ""}`}
+            className={`mb-4 text-lg text-white ${selectedOption === "option3" ? "text-blue-500" : ""}`}
             onClick={() => handleOptionClick("option3")}
           >
-            Option 3
+            <MdRadioButtonChecked />
           </button>
           <button
-            className={`mb-4 text-lg text-white ${selectedOption === "option4" ? "bg-blue-500" : ""}`}
-            onClick={() => handleOptionClick(<LoginForm />)}
+            className={`mb-4 text-lg text-white ${selectedOption === "LoginForm" ? "text-blue-500" : ""}`}
+            onClick={() => handleOptionClick("LoginForm")}
           >
-            Option 4
+            <MdInsertChartOutlined />
           </button>
         </div>
-        <div className="mb-4 flex flex-col items-center">
-          <button className="text-lg text-white">Settings</button>
-          <button className="text-lg text-white">Logout</button>
+        <div className="mb-4 flex flex-col items-center gap-5">
+          <button className="text-lg text-white">
+            <IoIosNotificationsOutline />
+          </button>
+          <button className="text-lg text-white">
+            <IoSettingsOutline />
+          </button>
+          <button className="text-lg text-white">
+            <Avatar alt="Remy Sharp" src="/public/avatar.jpg" />
+          </button>
         </div>
       </div>
-      <div className="flex w-full items-center justify-center ">
+      <div className="flex w-full items-center justify-center">
         {selectedOption && (
-          <div className=" text-blue to-blue-600 p-4">
-            <p>You selected: {selectedOption}</p>
+          <div className="bg-blue-600 p-4">
+            {selectedOption === "DataTable" && <DataTable />}
+            {selectedOption === "LoginForm" && <LoginForm />}
             <button
               className="text-blue"
               onClick={() => setSelectedOption(null)}
